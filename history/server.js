5aeb58d8895969eeb1b0fb907690a3a862b0af6a
dbdavis256 <127054796+dbdavis256@users.noreply.github.com>
2025-10-21 13:45:16 -0500

    remove unused response header and add comment

--- history/server.js
+++ history/server.js
@@ -1,3 +1,5 @@
+// run with "npm start"
+// listen to port 3000 and write the "t" and "u" querystring vars to history.txt
 import http from "http";
 import * as fs from 'fs';
 import { formatInTimeZone } from 'date-fns-tz';
@@ -7,8 +9,8 @@ const timeZone = 'America/Chicago';
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+  //res.setHeader("Content-Type", "text/plain");
+  //res.end("Hello World\n");
   const requestUrl = new URL(req.url, `http://${req.headers.host}`);
   const timeStamp = formatInTimeZone(new Date(), timeZone, 'yyyy-MM-dd HH:mm:ssXXX');
   const line = timeStamp + ' ' + requestUrl.searchParams.get('u') + ' "' + requestUrl.searchParams.get('t') + '"\n';
