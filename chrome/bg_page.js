2ed37e307348cf6a9201b7cdf759c3ee6d160642
dbdavis256 <127054796+dbdavis256@users.noreply.github.com>
2025-10-21 13:35:43 -0500

    Clarify bg_page.js purpose with a comment
    Added a comment to clarify the purpose of the bg_page.js file.

--- chrome/bg_page.js
+++ chrome/bg_page.js
@@ -1,3 +1,4 @@
+// bg_page.js an extension service worker to call fetch, avoiding CORS issues
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   fetch('http://localhost:3000?u=' + message.currentUrl + '&t=' + message.currentTitle);
 });
